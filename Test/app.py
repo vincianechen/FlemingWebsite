c10066267c63f0ef3f61990762348093eaee1b92
Vinci Chen <vincianechen@gmail.com>
2018-01-11 16:00:26 -0800

    Started on home page and navbar

--- Test/app.py
+++ Test/app.py
@@ -10,10 +10,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     if not session.get('logged_in'):
-        return render_template('login.html')
+        return render_template('home.html')
     else:
         return "Hello Boss!  <a href='/logout'>Logout</a>"
  
+
 @app.route('/login', methods=['POST'])
 def do_admin_login():
  
@@ -30,11 +31,13 @@ def do_admin_login():
         flash('wrong password!')
     return home()
  
-@app.route("/logout")
+
+@app.route('/logout')
 def logout():
     session['logged_in'] = False
     return home()
+    
  
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000)
+    app.run(debug=True)
